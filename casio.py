5d7769139d5e93fa1cc65ef461f1fef6f0cf3af3
Vpech77 <vanessa.pech77@gmail.com>
2025-01-23 23:44:46 +0100

    del test

--- casio.py
+++ casio.py
@@ -7,12 +7,9 @@ Created on Wed Jan 22 21:42:13 2025
 import matplotlib.pyplot as plt
 import numpy as np
 
-def f(x):
-    return 2*x + 2
-
-def draw_graph(f):
+def draw_graph(chaine):
     x = np.linspace(-10, 10, 400)
-    y = f(x)
+    y = parser(chaine, x)
 
     plt.plot(x, y, label='f(x)')
 
@@ -24,7 +21,7 @@ def draw_graph(f):
     plt.grid(True)
     
 def isOperator(car):
-    return car in "+-\*"
+    return car in "+-\*()"
 
 def isVar(car):
     return car == 'x'
@@ -48,66 +45,76 @@ def lexer(chaine):
         i += 1
     return tokens
 
-def magic(left, op, right):
-    return op(left, right)
-
-def multiply(a, b):
-    return a * b
-
 def addX(tokens, x):
     new_tokens = []
     i = 0
-    length = len(tokens)
     
-    while i < length:
+    while i < len(tokens):
         if isVar(tokens[i]):
-            if (i-1 >= 0 and isinstance(tokens[i-1], int)):
-                new_tokens.pop()
-                new_tokens.append(multiply(tokens[i-1] , x))
+            if i == 0:
+                new_tokens.append(x)
+            if i-1 >= 0:
+                if isinstance(tokens[i-1], int):
+                    new_tokens.pop()
+                    new_tokens.append(tokens[i-1]*x)
+                # x alone
+                else:
+                    new_tokens.append(x)
         else:
             new_tokens.append(tokens[i])
-            
         i += 1
-        
     return new_tokens
 
-def parser(tokens):
-    pass
-                
+# A = F (+ F)*
+
+def A(tokens, pos):
+    val, pos = F(tokens, pos)
+    while pos < len(tokens) and tokens[pos] == '+':
+        pos += 1
+        next_val, pos = F(tokens, pos)
+        val += next_val
+    return val, pos
+
+# F = N (* N)*
+
+def F(tokens, pos):
+    val, pos = N(tokens, pos)
+    while pos < len(tokens) and tokens[pos] == '*':
+        pos += 1
+        next_val, pos = N(tokens, pos)
+        val *= next_val
+    return val, pos
+
+# N = n | (A)
+
+def N(tokens, pos):
+    if pos < len(tokens) and isinstance(tokens[pos], (int, float, np.ndarray)):
+        val = tokens[pos]
+        return val, pos + 1
+    if pos < len(tokens) and tokens[pos] == '(':
+        val, pos = A(tokens, pos+1)
+        if pos < len(tokens) and tokens[pos] == ')':
+            return val, pos + 1
+
+def parser(chaine, x):
+    tokens = lexer(chaine)
+    tokens = addX(tokens, x)
+    print(tokens)
+
+    pos = 0
+    val, pos = A(tokens, pos)
+    return val
 
 if __name__ == "__main__":
     print("################## EQUATION ######################")
     
-    chaine = "22x+256+45"
+    chaine = "22x*x+45"
+    
     print(chaine)
-    tokens = lexer(chaine)
-
-    magic(5, (lambda a, b: a == b), 5)
     
-    tokens = addX(tokens, 2)
-    print("################## TOKENS ######################")
-    print(tokens)
+    val = parser(chaine, 2)
     
-    i = 0
-    length = len(tokens)
-    
-    op = []
-    nums = []
-    
-    while i < length:
-        if i<length and not(isinstance(tokens[i], int)):
-            op.append(tokens[i])
-        else:
-            nums.append(tokens[i])
-        i += 1
-    
-    print("################## NUM et OPERATOR ######################")
-    print(nums, op)            
-    
-    
-        
-    
-
+    draw_graph(chaine)
     
     
     
